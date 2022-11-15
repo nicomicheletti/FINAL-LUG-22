b9485e971e2b59206129b524a6d3ecc5549b69d2
Nicolas Micheletti <nicolasamicheletti@gmail.com>
2022-11-15 15:12:19 -0300

    modified product logic

--- controllers/product.ts
+++ controllers/product.ts
@@ -3,42 +3,33 @@ import Product from "../models/product"
 
 
 export const productController = {
-
-    // add producto
     add: async (req: Request, res: Response) => {
-
-        const newProd = new Product({ ...req.body })
-        newProd.save()
-            .then((cart) => {
-                if (!cart) {
-                    return res.status(404).send()
-                }
-                res.send(cart)
-            }).catch((err) => {
-                res.status(500).send(err)
-            })
+        try {
+            const newProd = new Product({ ...req.body })
+            var prod = await newProd.save();
+            res.send(prod);
+        } catch (error) {
+            res.status(500).send(error);
+        }
     },
 
-    // delete producto
     delete: async (req: Request, res: Response) => {
 
-        Product.findOneAndDelete({ _id: req.body.productId }).then((cart) => {
-            // Al hacer un find({}) pasandole un objeto vacio,trae toda la lista de carts
-            res.send(cart)
-        }).catch((err) => {
-            res.status(500).send(err)
-        })
+        try {
+            var product = await Product.findOneAndDelete({ _id: req.body.productId });
+            res.send(product);
+        } catch (error) {
+            res.status(500).send(error);
+        }
     },
 
-    // getAll productos
     getAll: async (req: Request, res: Response) => {
-        // cuando hago un find con el obj vacio({}) me trae una lista con todos los productos
-        Product.find({}).then((cart) => {
-            res.send(cart)
-        }).catch((err) => {
-            res.status(500).send(err)
-        })
+        try {
+            var prod = await Product.find({});
+            res.send(prod);
+        } catch (error) {
+            res.status(500).send(error)
+        }
+
     },
-
-
 };
